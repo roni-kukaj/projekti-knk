b1f13fa4cbd85d4dfe53205bc7e53a36e130f788
Roni <roni.kukaj@student.uni-pr.edu>
2023-04-16 16:46:52 +0200

    FInished the StudentView

--- Projekti-SRS/src/main/java/com/example/projektisrs/HelloApplication.java
+++ Projekti-SRS/src/main/java/com/example/projektisrs/HelloApplication.java
@@ -3,16 +3,20 @@ package com.example.projektisrs;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("StudentsView.fxml")
+        );
+        AnchorPane studentsView = fxmlLoader.load();
+        Scene scene = new Scene(studentsView);
+        stage.setTitle("Sistemi për Regjistrimin e Studentëve");
         stage.setScene(scene);
         stage.show();
     }
---
+++ Projekti-SRS/src/main/java/controllers/StudentsViewController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StudentsViewController {
+    @FXML
+    private TextField idFilterField;
+    @FXML
+    private TextField emriFilterField;
+    @FXML
+    private TextField mbiemriFilterField;
+    @FXML
+    private Button filterButton;
+
+
+}
