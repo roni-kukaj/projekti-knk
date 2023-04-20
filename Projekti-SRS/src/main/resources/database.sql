c9bbfcd3e383254d9f40d7e93e4d6aec2e7d266e
Roni <roni.kukaj@student.uni-pr.edu>
2023-04-20 22:53:12 +0200

    added files and folders for project structure

---
+++ Projekti-SRS/src/main/resources/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Studenti(
+    ID INTEGER NOT NULL AUTOINCREMENT, te filloje numerimin prej 230756000000
+    shto constraints qe drejtimi te jete IKS, TIK ose EAR
+);
+
+insert statments me vone
+Admin -> Qyeteti -> Shkolla -> Studenti
