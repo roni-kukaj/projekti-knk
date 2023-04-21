3bdb4aae10c753cb01977fed8dc08b7f90cddaae
Lorena Mekaj <lorenamekaj@gmail.com>
2023-04-21 16:51:37 +0200

    Made DashbordViewController

--- Projekti-SRS/src/main/java/controllers/DashbordViewControler.java
+++ Projekti-SRS/src/main/java/controllers/DashbordViewControler.java
@@ -1,5 +1,30 @@
 package controllers;
 
-public class DashbordViewControler {
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
 
-}
+public class DashbordViewControler {
+    private ImageView registerImageView;
+    private ImageView stulistImageView;
+    private ImageView updateImageView;
+    private ImageView graphicsImageView;
+    private ImageView settingImageView;
+
+
+    public void Clicked(ActionEvent e){
+
+        if (e.getSource() == registerImageView) {
+
+            System.out.println("Register image view clicked.");
+        } else if (e.getSource() == stulistImageView) {
+            System.out.println("Student list image view clicked.");
+        } else if (e.getSource() == updateImageView) {
+            System.out.println("Update image view clicked.");
+        } else if (e.getSource() == graphicsImageView) {
+            System.out.println("Graphics image view clicked.");
+        } else if (e.getSource() == settingImageView) {
+            System.out.println("Settings image view clicked.");
+        }
+    }
+    }
+
