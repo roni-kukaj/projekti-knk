5e8dd4be343a865250d25fd24f3f6409cb279394
Lorena Mekaj <lorenamekaj@gmail.com>
2023-04-21 16:57:43 +0200

    Made LoginVewController

--- Projekti-SRS/src/main/java/controllers/RegisterViewController.java
+++ Projekti-SRS/src/main/java/controllers/RegisterViewController.java
@@ -2,27 +2,67 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class RegisterViewController {
 
 
     public class LoginViewController {
+
+
+        private TextField emriTextfield;
         @FXML
-        private TextField usernameTextfield;
+        private TextField mbiemriTextfield;
         @FXML
-        private TextField passwordTextfield;
-        @FXML
-        private Button loginButton;
+        private RadioButton mRadioChoice;
+
+        private RadioButton fradioChoice;
+
+        private DatePicker birthdayPicker;
+
+        private ChoiceBox qytetiLindjesChoiceBox;
+
+        private TextField emailTextfield;
+
+        private ChoiceBox komunaChoiceBox;
+
+        private ChoiceBox shkollaChoiceBox;
+
+        private TextField maturaTextfield;
+
+        private TextField suksesiTextfield;
+
+        private TextField provimiPranuesTextfield;
+
+        private ChoiceBox drejtimiChoiceBox;
+
+        private Button registerButton;
 
         @FXML
-        private void loginButtonClicked(ActionEvent e){
-            String username = this.usernameTextfield.getText();
-            String password = this.passwordTextfield.getText();
+        private void registerButton(ActionEvent e){
+            String emri = this.emriTextfield.getText();
+            String mbiemri = this.mbiemriTextfield.getText();
+            String Mchoice = this.mRadioChoice.getText();
+            String Fchoice = this.fradioChoice.getText();
+            String birthdate = this.birthdayPicker.toString();
+            String choiceBox = this.qytetiLindjesChoiceBox.toString();
+            String email = this.emailTextfield.getText();
+            String komuna = this.komunaChoiceBox.toString();
+            String shkolla = this.shkollaChoiceBox.toString();
+            String matura = this.maturaTextfield.getText();
+            String suksesi = this.suksesiTextfield.getText();
+            String provimiPranues = this.provimiPranuesTextfield.getText();
+            String drejtimi = this.drejtimiChoiceBox.toString();
+            // String provimiPranues = this.provimiPranuesTextfield.getText();
 
-            System.out.println("Username: " + username + " - Password: " + password);
+
+            System.out.println("Emri: " + emri + " - Mbiemri: " + mbiemri);
+            System.out.println("Data e Lindjes: " + birthdate  + " - Email: " + email);
+            System.out.println("Testi i Matures: " + matura  + " - Suksesi ne SHM: " + suksesi + "Provimi pranues" + provimiPranues);
         }
+
+
+
     }
 
 }
