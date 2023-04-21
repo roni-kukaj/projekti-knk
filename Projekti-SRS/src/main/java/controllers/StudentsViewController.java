f0eea06ac5dd809e577d15f10c1bec1c4b88dd5a
Lorena Mekaj <lorenamekaj@gmail.com>
2023-04-21 18:46:45 +0200

    small changes SB

--- Projekti-SRS/src/main/java/controllers/StudentsViewController.java
+++ Projekti-SRS/src/main/java/controllers/StudentsViewController.java
@@ -14,7 +14,7 @@ public class StudentsViewController {
     private TextField mbiemriFilterField;
     @FXML
     private Button filterButton;
-    
+
 //    @FXML
 //    private TableView studentTable;
 
