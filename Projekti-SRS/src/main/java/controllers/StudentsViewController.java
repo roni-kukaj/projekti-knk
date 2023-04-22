0908eedb4e1ea53f3f7d9caaba6491d79d40e617
Roni <roni.kukaj@student.uni-pr.edu>
2023-04-23 01:13:46 +0200

    repositories

--- Projekti-SRS/src/main/java/controllers/StudentsViewController.java
+++ Projekti-SRS/src/main/java/controllers/StudentsViewController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.Studenti;
 
 public class StudentsViewController {
     @FXML
@@ -14,11 +18,24 @@ public class StudentsViewController {
     private TextField mbiemriFilterField;
     @FXML
     private Button filterButton;
-//    @FXML
-//    private TableView studentTable;
+    @FXML
+    private TableView<Studenti> studentTable;
+    @FXML
+    private TableColumn<Studenti, Integer> idColumn;
+    @FXML
+    private TableColumn<Studenti, String> emriColumn;
+    @FXML
+    private TableColumn<Studenti, String> mbiemriColumn;
+    @FXML
+    private TableColumn<Studenti, String> drejtimiColumn;
 
-//    @FXML
-//    private TableView studentTable;
+    private ObservableList<Studenti> studentiData;
 
+    public void initialize(){
+        idColumn.setCellValueFactory(new PropertyValueFactory<Studenti, Integer>("ID"));
+        emriColumn.setCellValueFactory(new PropertyValueFactory<Studenti, String>("Emri"));
+        mbiemriColumn.setCellValueFactory(new PropertyValueFactory<Studenti, String>("Mbiemri"));
+        drejtimiColumn.setCellValueFactory(new PropertyValueFactory<Studenti, String>("Drejtimi"));
 
+    }
 }
---
+++ Projekti-SRS/src/main/java/models/TableStudenti.java
@@ -0,0 +1,47 @@
+package models;
+
+public class TableStudenti {
+    private int id;
+    private String emri;
+    private String mbiemri;
+    private String drejtimi;
+
+    public TableStudenti(int id, String emri, String mbiemri, String drejtimi) {
+        this.id = id;
+        this.emri = emri;
+        this.mbiemri = mbiemri;
+        this.drejtimi = drejtimi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
+
+    public String getMbiemri() {
+        return mbiemri;
+    }
+
+    public void setMbiemri(String mbiemri) {
+        this.mbiemri = mbiemri;
+    }
+
+    public String getDrejtimi() {
+        return drejtimi;
+    }
+
+    public void setDrejtimi(String drejtimi) {
+        this.drejtimi = drejtimi;
+    }
+}
