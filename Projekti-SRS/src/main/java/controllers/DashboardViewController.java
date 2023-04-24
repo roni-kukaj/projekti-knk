ee355a615ed2769a5694435de57d0c4e6fc64263
Lorena Mekaj <lorenamekaj@gmail.com>
2023-04-25 00:38:06 +0200

    Made functions for controllers

--- Projekti-SRS/src/main/java/controllers/DashboardViewController.java
+++ Projekti-SRS/src/main/java/controllers/DashboardViewController.java
@@ -1,30 +1,75 @@
 package controllers;
 
 import javafx.event.ActionEvent;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import services.SceneUtil;
+
+import java.io.IOException;
+
 
 public class DashboardViewController {
 
-    private ImageView registerImageView;
-    private ImageView stulistImageView;
-    private ImageView updateImageView;
-    private ImageView graphicsImageView;
-    private ImageView settingImageView;
+    private Button registerStudentButton;
+    private Button studentListButton;
+    private Button updateStudentButton;
+    private Button graphicsButton;
+    private Button settingsButton;
 
 
-    public void Clicked(ActionEvent e){
-
-        if (e.getSource() == registerImageView) {
-            System.out.println("Register image view clicked.");
-        } else if (e.getSource() == stulistImageView) {
-            System.out.println("Student list image view clicked.");
-        } else if (e.getSource() == updateImageView) {
-            System.out.println("Update image view clicked.");
-        } else if (e.getSource() == graphicsImageView) {
-            System.out.println("Graphics image view clicked.");
-        } else if (e.getSource() == settingImageView) {
-            System.out.println("Settings image view clicked.");
+    public void registerClicked(ActionEvent e)throws IOException{
+        {
+            try{
+                SceneUtil.changeScene((Stage)this.registerStudentButton.getScene().getWindow(), "/com/example/projektisrs/RegisterView.fxml");
+            } catch(IOException ioe){
+                // TO DO
+            }
         }
     }
+
+    public void studentClicked(ActionEvent e)throws IOException{
+        {
+            try{
+                SceneUtil.changeScene((Stage)this.studentListButton.getScene().getWindow(), "/com/example/projektisrs/StudentsView.fxml");
+            } catch(IOException ioe){
+                // TO DO
+            }
+        }
     }
+
+
+    public void updateClicked(ActionEvent e)throws IOException{
+        {
+            try{
+                SceneUtil.changeScene((Stage)this.updateStudentButton.getScene().getWindow(), "/com/example/projektisrs/UpdateView.fxml");
+            } catch(IOException ioe){
+                // TO DO
+            }
+        }
+    }
+
+    public void graphicsClicked(ActionEvent e)throws IOException{
+        {
+            try{
+                SceneUtil.changeScene((Stage)this.graphicsButton.getScene().getWindow(), "/com/example/projektisrs/GraphicsView.fxml");
+            } catch(IOException ioe){
+                // TO DO
+            }
+        }
+    }
+
+
+    public void settingsClicked(ActionEvent e)throws IOException{
+        {
+            try{
+                SceneUtil.changeScene((Stage)this.settingsButton.getScene().getWindow(), "/com/example/projektisrs/SettingsView.fxml");
+            } catch(IOException ioe){
+                // TO DO
+            }
+        }
+    }
+
+
+}
+
 
