fa2b2b0be4a2593d5c4c9ff14eac98f9065897e1
MajlindaHalilaj <121357662+MajlindaHalilaj@users.noreply.github.com>
2023-04-27 01:13:20 +0200

    update SettingsViewController

--- Projekti-SRS/src/main/java/controllers/SettingsViewController.java
+++ Projekti-SRS/src/main/java/controllers/SettingsViewController.java
@@ -1,5 +1,16 @@
 package controllers;
 
-public class SettingsViewController {
+@FXML
+private Button goBackButton;
 
+public class SettingsViewController {
+    @FXML
+    public void goToDashboard() {
+        try{
+            SceneUtil.changeScene((Stage)this.goBackButton.getScene().getWindow(), "/com/example/projektisrs/DashboardView.fxml");
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+    }
 }
