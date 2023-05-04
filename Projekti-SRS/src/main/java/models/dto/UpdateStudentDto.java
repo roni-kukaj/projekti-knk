463f2ef13813ee80381856ec59e5961c399c0a97
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-05-04 23:44:27 +0200

    DTO changes

--- Projekti-SRS/src/main/java/models/dto/UpdateStudentDto.java
+++ Projekti-SRS/src/main/java/models/dto/UpdateStudentDto.java
@@ -15,26 +15,10 @@ public class UpdateStudentDto {
     private String email;
 
     private int id;
-    @FXML
-    private ChoiceBox drejtimiChoiceBox;
-    @FXML
-    private Button updateButton;
-    @FXML
-    private Button searchButton;
-    @FXML
-    private Button goBackButton;
 
-    @FXML
-    private void registerButton(ActionEvent e){
-        String emri = this.emriUpdateTextfield.getText();
-        String mbiemri = this.mbiemriUpdateTextfield.getText();
-        String email = this.emailTextfield.getText();
-        String id = this.idTextField.getText();
-        String drejtimi = this.drejtimiChoiceBox.toString();
-        // String provimiPranues = this.provimiPranuesTextfield.getText();
+    private String drejtimi;
 
+    public UpdateStudentDto(String emri, String mbiemri, String email, int id, String drejtimi){
 
-        System.out.println("Emri: " + emri + " - Mbiemri: " + mbiemri);
-        System.out.println("Email: " + email);
     }
 }
