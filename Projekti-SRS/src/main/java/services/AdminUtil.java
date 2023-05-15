f2cba2d7964537415a5ac83f315acb32aee8e732
Lorena Mekaj <lorenamekaj@gmail.com>
2023-05-15 23:36:52 +0200

    changes

--- Projekti-SRS/src/main/java/services/AdminUtil.java
+++ Projekti-SRS/src/main/java/services/AdminUtil.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class AdminUtil {
 
-    private Admin admin;
+    private static Admin admin;
     public static boolean login(String username, String password){
         try{
             Admin admin_ = AdminRepository.getAdminByUsername(username);
@@ -17,7 +17,7 @@ public class AdminUtil {
                 return false;
             }
             if(BCrypt.checkpw(password, admin_.getPassword())){
-                this.admin=admin_;
+                admin=admin_;
                 return true;
             }
             else{
@@ -31,16 +31,16 @@ public class AdminUtil {
     public static void saveNewPassword(String currentPassword, String newPassword, String confirmNewPassword) {
         try {
 
-            if (!this.admin.getPassword().equals(currentPassword)) {
+            if (!admin.getPassword().equals(currentPassword)) {
                 System.out.println("Current password is incorrect");
                 return;
             }
 
-            if (!this.admin.equals(confirmNewPassword)) {
+            if (!admin.equals(confirmNewPassword)) {
                 System.out.println("New password and confirm password do not match");
                 return;
             }
-            this.admin.setPassword(newPassword);
+            admin.setPassword(newPassword);
             System.out.println("Password changed successfully");
         }catch(IOException e){
 
