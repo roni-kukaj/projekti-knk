f8905ffa93cfe982a707373846a9fd79523b0f94
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-05-16 23:08:41 +0200

    UpdateViewController

---
+++ Projekti-SRS/src/main/java/controllers/UpdateViewController.java
@@ -0,0 +1,45 @@
+import controllers.BaseController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+public class UpdateViewController extends BaseController {
+    @FXML
+    private TextField emriUpdateTextfield;
+    @FXML
+    private TextField mbiemriUpdateTextfield;
+    @FXML
+    private TextField emailTextfield;
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private ChoiceBox<String> drejtimiChoiceBox;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button goBackButton;
+    @FXML
+    private Label searchWithID;
+    @FXML
+    private Label updateForm;
+    @FXML
+    private Label updateEmri;
+    @FXML
+    private Label updateMbiemri;
+    @FXML
+    private Label updateEmail;
+    @FXML
+    private Label updateDrejtimi;
+
+    private int id;
